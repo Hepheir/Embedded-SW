19efb60584e3d02f9de48c78a57a5676baaf53b3
Hepheir <hepheir@gmail.com>
2019-09-28 01:25:03 +0900

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,21 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "김동주, 이 곳에 잠들다..."
 
-while true ; do
-    git fetch origin/master
-    reslog=$(git log HEAD..origin/master --oneline)
-    if [ "${reslog}" != "" ] ; then
-        killall python
-
-        echo "Updating..."
-        git pull
-
-        echo "Launching Program!"
-        python python/index.py &
-
-    else
-        echo "No changes"
-        sleep 20
-    fi
-done
+rm -r Embedded-SW
+git clone https://github.com/Hepheir/Embedded-SW.git
+python Embedded-SW/python/index.py
