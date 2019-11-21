3907a4797bfba280e36bb679250f994bc1e90b57
Hepheir <hepheir@gmail.com>
2019-11-21 22:48:36 +0900

    yellow and red detect

--- python/index.py
+++ python/index.py
@@ -52,13 +52,16 @@ if __name__ == '__main__':
         # 0 64 128 192 255
         LV0, LV1, LV2, LV3, LV4 = (0, 64, 128, 192, 255)
 
-        _, hsv_s = cv2.threshold(hsv[:,:,1], LV3, 255, cv2.THRESH_BINARY)
-        _, yuv_v = cv2.threshold(yuv[:,:,2], LV1, 255, cv2.THRESH_BINARY_INV)
+        _, hsv_h_l1 = cv2.threshold(hsv[:,:,0], LV1, 255, cv2.THRESH_BINARY_INV)
+        _, hsv_s_h3 = cv2.threshold(hsv[:,:,1], LV3, 255, cv2.THRESH_BINARY)
+        
+        _, yuv_v_l1 = cv2.threshold(yuv[:,:,2], LV1, 255, cv2.THRESH_BINARY_INV)
 
-        yellow = cv2.bitwise_and(hsv_s, yuv_v)
+        yellow  = cv2.bitwise_and(hsv_s_h3, yuv_v_l1)
+        red     = cv2.bitwise_and(hsv_h_l1, hsv_s_h3)
 
         cv2.imshow('yellow', yellow)
-        cv2.imshow('s', hsv[:,:,1])
+        cv2.imshow('red', red)
 
 
 
