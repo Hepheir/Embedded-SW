d700c406c654ec24f58d4d6e2c76e3d9e3179b2f
Hepheir <hepheir@gmail.com>
2019-11-22 00:59:25 +0900

    color debug mod

---
+++ python/color.py
@@ -0,0 +1,68 @@
+import numpy as np
+import cv2
+
+
+# HSV
+YELLOW_ub = [  32, 255, 255 ]
+YELLOW_lb = [  20,  40, 112 ]
+
+RED_ub = [ 192, 255, 255 ]
+RED_lb = [ 156,  40, 112 ]
+
+#
+ref = {
+    'YELLOW' : 0,
+    'RED' : 1
+}
+
+hsv_ub = np.array([YELLOW_ub, RED_ub])
+hsv_lb = np.array([YELLOW_lb, RED_lb])
+
+# ******************************************************************
+
+def nothing(x):
+    pass
+
+def getMask(src, color):
+    mask = cv2.inRange(src, hsv_ub[color], hsv_lb[color])
+    mask = cv2.erode(mask, (3,3), iterations=1)
+    mask = cv2.dilate(mask, (3,3), iterations=1)
+    return mask
+
+
+def debugMode(video, resolution):
+    frame = None
+    upperb = None
+    lowerb = None
+
+    winname = 'DEBUG'
+    cv2.namedWindow(winname)
+    cv2.createTrackbar('max_h', winname, 255, 255, nothing)
+    cv2.createTrackbar('max_s', winname, 255, 255, nothing)
+    cv2.createTrackbar('max_v', winname, 255, 255, nothing)
+    cv2.createTrackbar('min_h', winname, 0, 255, nothing)
+    cv2.createTrackbar('min_s', winname, 0, 255, nothing)
+    cv2.createTrackbar('min_v', winname, 0, 255, nothing)
+
+    while True:
+        key = cv2.waitKey(1)
+        if (key == ord(' ')):
+            break
+
+        grab, now = video.read()
+        if not grab: break
+        else:
+            frame = cv2.resize(now, resolution)
+            cv2.imshow('CAM', frame)
+
+        upperb = np.array([cv2.getTrackbarPos('max_h', winname), cv2.getTrackbarPos('max_s', winname), cv2.getTrackbarPos('max_v', winname)])
+        lowerb = np.array([cv2.getTrackbarPos('min_h', winname), cv2.getTrackbarPos('min_s', winname), cv2.getTrackbarPos('min_v', winname)])
+            
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lowerb, upperb)
+        mask = cv2.erode(mask, (3,3), iterations=2)
+        mask = cv2.dilate(mask, (3,3), iterations=2)
+
+
+        cv2.imshow('CAM', frame)
+        cv2.imshow(winname, mask)
--- python/index.py
+++ python/index.py
@@ -9,32 +9,9 @@ import math
 
 #-----------  0:노란색, 1:빨강색, 3:파란색
 
-COLOR_RED   = 0
-COLOR_BROWN = 1
-COLOR_RED2  = 2
-COLOR_GREEN = 3
-COLOR_BLUE  = 4
-    
-hsv_max = np.array([
-    [252, 65,196,111,110],  # H
-    [194,200,223,110,255],  # S
-    [255,151,239,156,255]]) # V
-
-hsv_min = np.array([
-    [150,  0,158, 59, 74],  # H
-    [113,140,150, 51,133],  # S
-    [ 89, 95,104, 61,104]]) # V
+import color as COLOR
     
 min_area =  [  50, 50, 50, 10, 10]
-
-
-def findColor(src, color):
-    global hsv_max, hsv_min
-    mask = cv2.inRange(src, hsv_min[:,color], hsv_max[:,color])
-    # mask = cv2.erode(mask, None, iterations=1)
-    # mask = cv2.dilate(mask, None, iterations=1)
-    return mask
-
 
 if __name__ == '__main__':
     BPS = 4800
@@ -77,7 +54,7 @@ if __name__ == '__main__':
         
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
  
-        mask = findColor(hsv, color)
+        mask = COLOR.getMask(hsv, color)
         cv2.imshow('mask', mask)
 
         key = cv2.waitKey(1)
@@ -92,21 +69,8 @@ if __name__ == '__main__':
             color += 1
             print('color', color)
 
-        # _, hsv_h_l1 = cv2.threshold(hsv[:,:,0], 32, 255, cv2.THRESH_BINARY_INV)
-        # _, hsv_s_h3 = cv2.threshold(hsv[:,:,1], 128, 255, cv2.THRESH_BINARY)
-        
-        # _, yuv_v_l1 = cv2.threshold(yuv[:,:,2], 32, 255, cv2.THRESH_BINARY_INV)
-
-        # yellow  = cv2.bitwise_and(hsv_s_h3, yuv_v_l1)
-        # red     = cv2.bitwise_xor(yellow, cv2.bitwise_and(hsv_h_l1, hsv_s_h3))
-
-        # cv2.imshow('yellow', yellow)
-        # cv2.imshow('hsv_h_l1', hsv_h_l1)
-        # cv2.imshow('red', red)
-
-
-
-
+        elif (key == ord('d')):
+            COLOR.debugMode(video, RESOLUTION)
         
 # ******************************************************************
 # ******************************************************************
