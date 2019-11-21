a5031632e4811b3617461a5683eab37a55485c02
Hepheir <hepheir@gmail.com>
2019-11-21 22:04:51 +0900

    .

--- python/index.py
+++ python/index.py
@@ -7,47 +7,54 @@ import cv2
 import time
 import math
 
-
 if __name__ == '__main__':
     BPS = 4800
-    RESOLUTION = (80, 60)
+    RESOLUTION = (120, 90)
     serial_use = False
     serial_port = None
 
+    # Load serial module
     if serial_use:
         serial_port = serial.Serial('/dev/ttyAMA0', BPS, timeout=0.001)
         serial_port.flush() # serial cls
-        print('Serial enabled.')
+        print('Serial \tenabled.')
     else:
-        print('Serial disabled.')
+        print('Serial \tdisabled.')
 
+    # Load camera
     video = cv2.VideoCapture(0)
     if video.isOpened():
         video.set(cv2.CAP_PROP_FRAME_WIDTH,  RESOLUTION[0])
         video.set(cv2.CAP_PROP_FRAME_HEIGHT, RESOLUTION[1])
-        print('Camera loaded.')
+        print('Camera \tloaded.')
     else:
         raise Exception("Could not open video device")
     
+# ******************************************************************
+# ******************************************************************
+# ******************************************************************
     print('Start mainloop.')
     while True:
         frame = None
-
+        
+        # Read a new frame
         grab, now = video.read()
         if not grab: break
         else:
             frame = cv2.resize(now, RESOLUTION)
             cv2.imshow('CAM', frame)
 
-        
+        # 
         yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
-        cv2.imshow('yuv', yuv)
-
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        cv2.imshow('nor hsv', cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR))
-        hsv[:,:,1] = 255*np.ones(RESOLUTION[-1:0])
-        cv2.imshow('adj hsv', cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR))
+
+        y = yuv[:,:,0]
+
+        cv2.imshow('Y', y)
 
         cv2.waitKey(1)
         
+# ******************************************************************
+# ******************************************************************
+# ******************************************************************
         
