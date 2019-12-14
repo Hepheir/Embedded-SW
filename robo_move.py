1774299284ed115d5e9c5eaafe0759b86d94429f
Hepheir <hepheir@gmail.com>
2019-12-14 11:55:39 +0900

    Update robo_move.py

--- robo_move.py
+++ robo_move.py
@@ -186,7 +186,7 @@ def isFoundObstacles(cmask):
 # -----------------------------------------------
 def dirCalibration(mask):
     ltr_turn_sen    = 24
-    ltr_shift_sen   = 20
+    ltr_shift_sen   = 22
     # v 로봇 카메라가 표시할 수 있는 최하단으로 부터, 가상으로 화면을 확장 시켰다고 가정시, 추정되는 로봇으로 부터 땅에 내린 수선의 발 위치
     bottom_y_ext    = cam.CENTER[1] * 2/3
     
